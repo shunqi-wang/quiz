45e16254c56dc5e2cad9548a3c5eaed7d64f1e51
shunqi-wang <iwsq98@gmail.com>
2018-02-22 09:14:56 +0800

    Some changes
    Some changes.

--- src/main/java/com/example/quiz/MainActivity.java
+++ src/main/java/com/example/quiz/MainActivity.java
@@ -1,33 +1,76 @@
 package com.example.quiz;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.*;
 
 import java.util.*;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button simpleAdapterBtn;
+    private ListView questionList;
+    private SimpleAdapter adapter;
+    private ArrayList<Map<String,Object>> data=new ArrayList<>();
+    private ArrayList<Question> ql=new ArrayList<>();
+    private Map<String,Object> map=new HashMap<String,Object>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button addBtn=(Button)findViewById(R.id.add);
+        Button saveBtn=(Button)findViewById(R.id.save);
+        addBtn.setOnClickListener(this);
+        saveBtn.setOnClickListener(this);
 
-        simpleAdapterBtn=(Button)findViewById(R.id.simpleAdapterBtn);
-        simpleAdapterBtn.setOnClickListener(simpleAdapterBtnListener);
+        Question question=new Question(1);
+        map=new HashMap<String,Object>();
+        map.put("title",question.getQuestionTitle());
+        data.add(map);
+        ql.add(question);
+
+        questionList=(ListView)findViewById(R.id.questionList);
+        adapter=new SimpleAdapter(this,data,R.layout.vlist,
+                new String[]{"title"},
+                new int[]{R.id.title});
+        questionList.setAdapter(adapter);
     }
 
-    private View.OnClickListener simpleAdapterBtnListener=new View.OnClickListener(){
-        public void onClick(View v){
-            Log.v("MyListView","simpleAdapter");
-            Intent intent=new Intent(MainActivity.this,MyListView.class);
-            startActivity(intent);
+
+    class Question{
+        private int number;
+        private String type;
+        private String description;
+
+        Question(int n){number=n;}
+        void setNumber(int n){number=n;}
+        void setType(String t){type=t;}
+        void setDescription(String d){description=d;}
+        int getNumber(){return number;}
+        String getType(){return type;}
+        String getDescription(){return description;}
+        String getQuestionTitle(){return "Question"+number;}
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.add:
+                Question question=new Question(adapter.getCount()+1);
+                map=new HashMap<String,Object>();
+                map.put("title",question.getQuestionTitle());
+                data.add(map);
+                ql.add(question);
+                adapter.notifyDataSetChanged();
+                break;
+            case R.id.save:
+                break;
         }
-    };
+    }
 }
